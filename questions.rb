3409ac9475f51b7ead6b2acb1e0a93ee28f4bea0
danjocutler <danjocutler@gmail.com>
2014-10-31 23:28:07 +0000

    21 questions completed

--- questions.rb
+++ questions.rb
@@ -67,8 +67,7 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
-	odd,even = array.group_by(&:odd?).values
-	return even,odd
+	array.partition {|number| number.even?}
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -76,7 +75,7 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
-
+	array.select {|word| word == word.reverse}.length
 end
 
 # return the shortest word in an array
@@ -109,7 +108,7 @@ end
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 33
 def average_of_array(array)
-	
+	(array.inject(&:+)/(array.length.to_f)).ceil
 end
 
 # get all the elements in an array, up until the first element
@@ -117,12 +116,14 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+	array.take_while {|number| number < 6}
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+	array.each_slice(2).to_h
 end
 
 # get all the letters used in an array of words and return
@@ -136,6 +137,7 @@ end
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+
 end
 
 # in a hash where the keys and values are all numbers
