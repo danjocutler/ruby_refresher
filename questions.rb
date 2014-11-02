a4fcf45355e1aa7255349b2522ab77fb31f71773
danjocutler <danjocutler@gmail.com>
2014-11-02 19:52:03 +0000

    5 more left (btw, had to change boolean tests to 'is_truthy', and 'is_falsey' for them to pass -- rspec 3.xx+)

--- questions.rb
+++ questions.rb
@@ -192,7 +192,7 @@ end
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
-
+	string.index( /[^[:alnum:]]/ )
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
@@ -204,6 +204,7 @@ end
 # should return true for a 3 dot range like 1...20, false for a 
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+	range.exclude_end?
 end
 
 # get the square root of a number
@@ -213,6 +214,7 @@ end
 
 # count the number of words in a file
 def word_count_a_file(file_path)
+	File.open(file_path).read.split(' ').count
 end
 
 # --- tougher ones ---
@@ -221,6 +223,7 @@ end
 # called call_method_from_string('foobar')
 # the method foobar should be invoked
 def call_method_from_string(str_method)
+	method(str_method)
 end
 
 # return true if the date is a uk bank holiday for 2014
